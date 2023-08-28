7e54ca9e50e7084638b1bfc2c9820b8e5b099d7d
unknown <julia.mejia@labinfo.is.escuelaing.edu.co>
2023-08-28 12:34:32 -0500

    todo funciona menos lo de la imagen

---
+++ target/classes/script.js
@@ -0,0 +1,1 @@
+function boton() = alert("hello world, my name is Julia")
---
+++ target/site/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"Cache"},{"p":"org.example","l":"HttpConnection"},{"p":"org.example","l":"HttpServer"}];updateSearchResults();
